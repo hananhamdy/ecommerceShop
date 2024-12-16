06c92022bb24e8c3d6b87bab144fcdeca6858279
HananHamdy <HananHamdy@blackstoneeit.com>
2024-12-16 13:44:44 +0200

    develop header and footer

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { User } from '../../../core/models/user.model';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  user: User | null = null;
+  isMenuOpen = false;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    this.checkScreenSize();
+  }
+
+  // Listen for window resize events
+  @HostListener('window:resize', [])
+  onResize() {
+    this.checkScreenSize();
+  }
+
+  // Function to check screen size and update isMenuOpen
+  private checkScreenSize(): void {
+    this.isMenuOpen = window.innerWidth >= 1024;
+  }
+
+  // toggle menu button on mobile view
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 
 }
